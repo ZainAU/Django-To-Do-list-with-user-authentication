a7f875fb9b5dc177f5903ec3970c5ecf0ea999dc
omemarizvi <77575949+omemarizvi@users.noreply.github.com>
2023-03-31 00:38:39 +0500

    Add files via upload

--- base/models.py
+++ base/models.py
@@ -1,18 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
+def one_week_hence():
+    return timezone.now() + timezone.timedelta(days=7)
 
 class Task(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
-
+    deadline = models.DateTimeField(default=one_week_hence)
+    status = models.BooleanField(default=False)
+    
     def __str__(self):
+
         return self.title
 
     class Meta:
-        order_with_respect_to = 'user'
+        order_with_respect_to = 'deadline'
