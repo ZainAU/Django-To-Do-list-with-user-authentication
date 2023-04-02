9e32a61de2e999690f200ac02b9f21064709163f
Zain Ahmed Usmani <46748807+ZainAU@users.noreply.github.com>
2023-04-02 20:00:53 +0500

    updated readme

--- base/forms.py
+++ base/forms.py
@@ -15,4 +15,4 @@ class UserCreateForm(UserCreationForm):
 
         for fieldname in ['username', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
-    print("oye we using this forms.py file")
+        # print("oye we using this forms.py file")
